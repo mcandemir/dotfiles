e8493a565bedadf2122dc67a8803d2cfc53b3179
Can <55599562+mcandemir@users.noreply.github.com>
2024-07-22 12:43:40 +0300

    Create archsetup.sh

---
+++ archsetup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ `id -u` -ne 0 ]
+  then echo "Run this script as root or using sudo."
+  exit
+fi
+
+echo "Enter a hostname: "
+read hostname
+
+echo "Updating Arch.."
+pacman -Syu
+
+
+echo "Installing packages.."
+IndexedArray=(git neovim zsh neofetch tmux alacritty)
+
+for i in "${IndexedArray[@]}"; do
+    echo "$i";
+
+    if pacman -Qi "$i" &> /dev/null; then
+        echo "Package $i Exists"
+    else
+        pacman -S $i --noconfirm
+    fi
+done
+
+
+echo "Copying config files.."
+cp -r /home/$hostname/dotfiles/ /home/$hostname
+
+
+echo "Installation complete."
+exit 0
